c48e960b9f829227962c8bab3dd0c826d23b6d60
Samuel <samuel.fahrngruber@gmail.com>
2018-10-07 15:54:12 +0200

    added DailyScrumTable

--- scrumble-desktop/scrumble/MainWindow.xaml.cs
+++ scrumble-desktop/scrumble/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using CefSharp;
 using System.IO;
 using CefSharp.Wpf;
 using CefSharp.SchemeHandler;
+using scrumble.DailyScrumTable;
 
 namespace scrumble
 {
@@ -50,7 +51,6 @@ namespace scrumble
             teamMembers.Add(new User("pauli"));
             teamMembers.Add(new User("simsi"));
             teamMembers.Add(new User("webi"));
-            teamMembers.Add(new User("sami"));
             treeViewItem_teamMembers.ItemsSource = teamMembers;
 
             string projectLog = "" +
@@ -58,6 +58,8 @@ namespace scrumble
                 "[2018-10-04 19:10] Successfully Tested\n" +
                 "[2018-10-04 19:30] Added Daily Scrum Table in Gui";
             textBox_projectLog.Text = projectLog;
+
+            initDailyScrumTable();
 
         }
 
@@ -78,6 +80,121 @@ namespace scrumble
             if (chromiumWebBrowser_scrumBoard.IsBrowserInitialized)
             {
                 //chromiumWebBrowser_scrumBoard.LoadHtml(File.ReadAllText("./Scrumboard/sbMarkup.html"));
+            }
+        }
+
+        private void initDailyScrumTable()
+        {
+            List<TableColumn> table = new List<TableColumn>();
+
+            TableColumn dayCol = new TableColumn();
+            dayCol.Header = "";
+
+            dayCol.Entries.Add(new DailyScrumRowHeader() { Text = "pauli" });
+            dayCol.Entries.Add(new DailyScrumRowHeader() { Text = "simsi" });
+            dayCol.Entries.Add(new DailyScrumRowHeader() { Text = "webi" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "Today";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#00", Text = "Say Hello" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#01", Text = "Say World" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#02", Text = "Say Something" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "Yesterday";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#10", Text = "Say Hello2" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#11", Text = "Say World2" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#12", Text = "Say Something2" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "2 Days Ago";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#20", Text = "Say Hello3" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#21", Text = "Say World3" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#22", Text = "Say Something3" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "3 Days Ago";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#30", Text = "Say Hello4" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#31", Text = "Say World4" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#32", Text = "Say Something4" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "4 Days Ago";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#40", Text = "Say Hello5" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#41", Text = "Say World5" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#42", Text = "Say Something5" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "5 Days Ago";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#50", Text = "Say Hello6" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#51", Text = "Say World6" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#52", Text = "Say Something6" });
+
+            table.Add(dayCol);
+
+            dayCol = new TableColumn();
+            dayCol.Header = "6 Days Ago";
+
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#60", Text = "Say Hello3" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#61", Text = "Say World3" });
+            dayCol.Entries.Add(new DailyScrumEntry() { TaskId = "#62", Text = "Say Something3" });
+
+            table.Add(dayCol);
+
+            itemsControl_DailyScrumDays.ItemsSource = table;
+        }
+
+        //private class TableCell<T>
+        //{
+        //    public T Value { get; set; }
+        //    public TableCell() : this(default(T))
+        //    {
+
+        //    }
+        //    public TableCell(T val)
+        //    {
+        //        Value = val;
+        //    }
+        //}
+
+        private class TableColumn
+        {
+            public string Header { get; set; }
+            public List<IDailyScrumCell> Entries { get; private set; }
+
+            public TableColumn()
+            {
+                Entries = new List<IDailyScrumCell>();
+            }
+        }
+
+        private class Table
+        {
+            public List<string> RowHeaders { get; private set; }
+            public List<TableColumn> TableColumns { get; private set; }
+
+            public Table()
+            {
+                RowHeaders = new List<string>();
+                TableColumns = new List<TableColumn>();
             }
         }
     }
