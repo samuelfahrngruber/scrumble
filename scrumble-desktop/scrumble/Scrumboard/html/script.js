f328d797d26d761e98d9443d93022e65cae202f3
Samuel <samuel.fahrngruber@gmail.com>
2018-10-08 15:06:51 +0200

    scrumboard tweaks

--- scrumble-desktop/scrumble/Scrumboard/html/script.js
+++ scrumble-desktop/scrumble/Scrumboard/html/script.js
@@ -3,14 +3,18 @@
         connectWith: ".column",
         handle: ".portlet-header",
         cancel: ".portlet-toggle",
-        placeholder: "portlet-placeholder ui-corner-all"
+        placeholder: "portlet-placeholder ui-corner-all",
+        stop: (event, ui) => {
+            var index = ui.item.index();
+            var column = ui.item.parent();
+            console.log("new position of " + ui.item.attr("id") + ": (column: " + column.attr("id") + ", row: " + index + ")");
+        }
     });
 
     $(".portlet")
       .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
       .find(".portlet-header")
-        .addClass("ui-widget-header ui-corner-all")
-
+      .addClass("ui-widget-header ui-corner-all")
 
     $(".portlet-toggle").click(function () {
         var icon = $(this);
